bfdac0002ddd2ee4b46704f2ca7d5f25237f7d99
Naqqash Khan <naqqash10@hotmail.com>
2020-04-09 19:36:38 +0500

    Updated.

--- ConsoleApp/Program.cs
+++ ConsoleApp/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using MRRCManagement;
 
 namespace ConsoleApp
 {
@@ -10,6 +11,8 @@ namespace ConsoleApp
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n### Mates-Rates Rent-a-Car Operation Menu ###\n");
+            Console.WriteLine("You may press the ESC key at any menu to exit. Press the BACKSPACE key to return to the previous menu\n");
         }
     }
 }
--- MRRC/Program.cs
+++ MRRC/Program.cs
@@ -10,7 +10,80 @@ namespace MRRC
     {
         static void Main(string[] args)
         {
+            Console.WriteLine("\n### Mates-Rates Rent-a-Car Operation Menu ###\n");
+            Console.WriteLine("You may press the ESC key at any menu to exit. Press the BACKSPACE key to return to the previous menu");
+            while (true) 
+            {
+                ConsoleKeyInfo mainInput;
+                Console.WriteLine("\nPlease enter a character from the options below:\n");
+                Console.WriteLine("a) Customer Management");
+                Console.WriteLine("b) Fleet Management");
+                Console.WriteLine("c) Rental Management");
+                Console.WriteLine();
+                Console.Write(">");
+                mainInput = Console.ReadKey();
+                Console.WriteLine();
+                if (mainInput.Key == ConsoleKey.A)
+                {
+                    while (true) 
+                    {
+                        ConsoleKeyInfo customerInput;
+                        Console.WriteLine("\nPlease enter a character from the options below:\n");
+                        Console.WriteLine("a) Display Customers");
+                        Console.WriteLine("b) New Customer");
+                        Console.WriteLine("c) Modify Customer");
+                        Console.WriteLine("d) Delete Customer");
+                        Console.WriteLine();
+                        Console.Write(">");
+                        customerInput = Console.ReadKey();
+                        Console.WriteLine();
+                        if (customerInput.Key == ConsoleKey.A)
+                        {
 
+                        }
+                        else if (customerInput.Key == ConsoleKey.B) 
+                        {
+
+                        }
+                        else if (customerInput.Key == ConsoleKey.C)
+                        {
+
+                        }
+                        else if (customerInput.Key == ConsoleKey.D)
+                        {
+
+                        }
+                        else if (customerInput.Key == ConsoleKey.Backspace)
+                        {
+                            break;
+                        }
+                        else if (customerInput.Key == ConsoleKey.Escape)
+                        {
+                            Environment.Exit(0);
+                        }
+                    }
+                }
+                else if (mainInput.Key == ConsoleKey.B)
+                {
+
+                }
+                else if (mainInput.Key == ConsoleKey.C)
+                {
+
+                }
+                else if (mainInput.Key == ConsoleKey.Escape)
+                {
+                    Environment.Exit(0);
+                }
+                else if (mainInput.Key == ConsoleKey.Backspace)
+                {
+                    //Do nothing
+                }
+                else 
+                {
+                    Console.WriteLine("\nInvalid Input");
+                }
+            }
         }
     }
 }
